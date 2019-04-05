28d26482d38e28d5436a2bb00a7f463c2dce9be9
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-04-05 21:55:27 +0200

    parameter grades put into the regulate() method.

--- src/main/java/org/lasencinas/interfaces/Regulable.java
+++ src/main/java/org/lasencinas/interfaces/Regulable.java
@@ -4,5 +4,5 @@ import org.lasencinas.room.RoomTemperature;
 
 public interface Regulable {
     void regulate(Temperable temperable, Heatable heatable, double minTemp, double maxTemp,
-                  RoomTemperature roomTemperature);
+                  RoomTemperature roomTemperature, double grades);
 }

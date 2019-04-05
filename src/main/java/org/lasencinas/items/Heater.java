a23cd056cd65f01f9b5bbe66fd8cb4183b7b0cc7
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-04-05 21:56:31 +0200

    Overrides deleted.

--- src/main/java/org/lasencinas/items/Heater.java
+++ src/main/java/org/lasencinas/items/Heater.java
@@ -3,15 +3,4 @@ package org.lasencinas.items;
 import org.lasencinas.interfaces.Heatable;
 
 public class Heater implements Heatable {
-
-    /* ---- Behaviours ---- */
-    @Override
-    public void engage() {
-
-    }
-
-    @Override
-    public void disengage() {
-
-    }
 }

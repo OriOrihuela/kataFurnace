c4f1e008ea4c5a9300eb8c7c0fcb77f7139ae3d4
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-04-05 20:15:07 +0200

    Methods done.

--- src/main/java/org/lasencinas/interfaces/Regulable.java
+++ src/main/java/org/lasencinas/interfaces/Regulable.java
@@ -1,4 +1,8 @@
 package org.lasencinas.interfaces;
 
+import org.lasencinas.room.RoomTemperature;
+
 public interface Regulable {
+    void regulate(Temperable temperable, Heatable heatable, double minTemp, double maxTemp,
+                  RoomTemperature roomTemperature);
 }

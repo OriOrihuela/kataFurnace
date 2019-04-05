1866b62c593211726ff222af7506c80f5940c730
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-04-05 21:56:51 +0200

    engage() and disengage() made as default methods.

--- src/main/java/org/lasencinas/interfaces/Heatable.java
+++ src/main/java/org/lasencinas/interfaces/Heatable.java
@@ -1,6 +1,17 @@
 package org.lasencinas.interfaces;
 
+import org.lasencinas.room.RoomTemperature;
+
 public interface Heatable {
-    void engage();
-    void disengage();
+
+    /* ---- Behaviours ---- */
+    default double engage(RoomTemperature roomTemperature, double grades) {
+        roomTemperature.addGrades(grades);
+        return roomTemperature.getTemperature();
+    }
+
+    default double disengage(RoomTemperature roomTemperature, double grades) {
+        roomTemperature.subtractGrades(grades);
+        return roomTemperature.getTemperature();
+    }
 }

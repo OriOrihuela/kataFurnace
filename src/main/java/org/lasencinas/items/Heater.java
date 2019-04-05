16e102d9b8b9e773d0c208f2ab38674835ea2335
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-04-05 20:13:12 +0200

    Methods implemented.

--- src/main/java/org/lasencinas/items/Heater.java
+++ src/main/java/org/lasencinas/items/Heater.java
@@ -3,4 +3,16 @@ package org.lasencinas.items;
 import org.lasencinas.interfaces.Heatable;
 
 public class Heater implements Heatable {
+
+    /* ---- Behaviours ---- */
+
+    @Override
+    public void engage() {
+
+    }
+
+    @Override
+    public void disengage() {
+
+    }
 }

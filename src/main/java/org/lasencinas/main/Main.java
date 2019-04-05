f8ae69619c4fa4d91b6f37a57695a10dff9cb292
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-04-05 20:15:26 +0200

    Main changed to pass my classes.

--- src/main/java/org/lasencinas/main/Main.java
+++ src/main/java/org/lasencinas/main/Main.java
@@ -1,36 +1,31 @@
-package org.lasencinas.main.
+package org.lasencinas.main;
 
-import org.foobarspam.furnaceDIP.hardware.GasHeater;
-import org.foobarspam.furnaceDIP.hardware.Regulator;
-import org.foobarspam.furnaceDIP.hardware.RemoteCommandSensor;
-import org.foobarspam.furnaceDIP.interfaces.Heater;
-import org.foobarspam.furnaceDIP.interfaces.Thermometer;
-import org.foobarspam.furnaceDIP.otherstuff.Jedi;
-import org.foobarspam.furnaceDIP.types.RoomTemperature;
 
-/**
- * Hello world!
- *
- */
-public class App
-{
-    public static void main( String[] args )
-    {
+import org.lasencinas.interfaces.Heatable;
+import org.lasencinas.interfaces.Temperable;
+import org.lasencinas.person.Girl;
+import org.lasencinas.items.Heater;
+import org.lasencinas.items.Regulator;
+import org.lasencinas.items.Thermometer;
+import org.lasencinas.room.RoomTemperature;
+
+public class Main {
+    public static void main(String[] args) {
         final double minTemp = 15.0;
         final double maxTemp = 21.0;
 
         RoomTemperature temperature = new RoomTemperature(15);
-        Heater heater = new GasHeater();
-        Thermometer thermometer = new RemoteCommandSensor();
+        Heatable heater = new Heater();
+        Temperable thermometer = new Thermometer();
 
         Regulator regulator = new Regulator();
 
-        System.out.println( "Arrancando..." );
+        System.out.println("Arrancando...");
         regulator.regulate(thermometer, heater, minTemp, maxTemp, temperature);
 
-        Jedi yoda = new Jedi();
-        System.out.println( "\nArrancando a Yoda: " );
-        regulator.regulate(thermometer, yoda, minTemp, maxTemp, temperature);
-        yoda.speak();
+        Girl moza = new Girl();
+        System.out.println("\nArrancando a la moza: ");
+        regulator.regulate(thermometer, moza, minTemp, maxTemp, temperature);
+        moza.speak();
     }
 }

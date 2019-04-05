349bb264597e99b0698fac161729b243265ea87d
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-04-05 21:57:05 +0200

    Overrides deleted.

--- src/main/java/org/lasencinas/person/Girl.java
+++ src/main/java/org/lasencinas/person/Girl.java
@@ -5,16 +5,6 @@ import org.lasencinas.interfaces.Heatable;
 public class Girl implements Heatable {
 
     /* ---- Behaviours ---- */
-    @Override
-    public void disengage() {
-
-    }
-
-    @Override
-    public void engage() {
-
-    }
-
     public void speak() {
         System.out.println("The girl says: " + '\n' +
                 "   Oh my gosh!");

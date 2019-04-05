326819971ed162a107ea1c0d20862cf8f9984f16
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-04-05 21:55:41 +0200

    Override deleted.

--- src/main/java/org/lasencinas/person/Man.java
+++ src/main/java/org/lasencinas/person/Man.java
@@ -5,11 +5,6 @@ import org.lasencinas.interfaces.Temperable;
 public class Man implements Temperable {
 
     /* ---- Behaviours ---- */
-    @Override
-    public void read() {
-
-    }
-
     public void speak() {
         System.out.println("The man says: " + '\n' +
                 "   Let's do it!" + '\n');

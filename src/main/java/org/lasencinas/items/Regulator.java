fbae40565bcf019037d4d094526bf8d9294045e9
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-04-05 20:13:36 +0200

    Method implemented.

--- src/main/java/org/lasencinas/items/Regulator.java
+++ src/main/java/org/lasencinas/items/Regulator.java
@@ -1,6 +1,15 @@
 package org.lasencinas.items;
 
+import org.lasencinas.interfaces.Heatable;
 import org.lasencinas.interfaces.Regulable;
+import org.lasencinas.interfaces.Temperable;
+import org.lasencinas.room.RoomTemperature;
 
 public class Regulator implements Regulable {
+
+    /* ---- Behaviours ---- */
+    public void regulate(Temperable temperable, Heatable heatable, double minTemp, double maxTemp,
+                         RoomTemperature roomTemperature) {
+
+    }
 }

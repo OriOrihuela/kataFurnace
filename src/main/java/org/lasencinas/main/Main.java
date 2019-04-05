75853503745d51988e02ca97c5219aabc67fd16f
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-04-05 21:56:00 +0200

    Main refactored :D

--- src/main/java/org/lasencinas/main/Main.java
+++ src/main/java/org/lasencinas/main/Main.java
@@ -19,26 +19,69 @@ public class Main {
         /* If we try to create another RoomTemperature instance, a message will be displayed alerting us it will not
            be possible
          */
-        System.out.println(temperature.getTemperature());
 
         Heatable heater = new Heater();
         Temperable thermometer = new Thermometer();
 
         Regulator regulator = new Regulator();
-        regulator.regulate(thermometer, heater, minTemp, maxTemp, temperature);
+        regulator.regulate(thermometer, heater, minTemp, maxTemp, temperature, 0.5);
+
+        /**
+         * Calentando => temperatura 15.5
+         * Calentando => temperatura 16.0
+         * Calentando => temperatura 16.5
+         * Calentando => temperatura 17.0
+         * Calentando => temperatura 17.5
+         * Calentando => temperatura 18.0
+         * Calentando => temperatura 18.5
+         * Calentando => temperatura 19.0
+         * Calentando => temperatura 19.5
+         * Calentando => temperatura 20.0
+         * Calentando => temperatura 20.5
+         * Calentando => temperatura 21.0
+         * Apagado => temperatura 20.5
+         * Apagado => temperatura 20.0
+         * Apagado => temperatura 19.5
+         * Apagado => temperatura 19.0
+         * Apagado => temperatura 18.5
+         * Apagado => temperatura 18.0
+         * Apagado => temperatura 17.5
+         * Apagado => temperatura 17.0
+         * Apagado => temperatura 16.5
+         * Apagado => temperatura 16.0
+         * Apagado => temperatura 15.5
+         * Apagado => temperatura 15.0
+         */
 
         Man gañán = new Man();
         Girl moza = new Girl();
 
-        regulator.regulate(gañán, moza, minTemp, maxTemp, temperature);
-        /* In the line of code above, it is demonstrated that dependency injection works with 2 different objects
-        with the same interface implementation.
-         */
-
         System.out.println("Precalentando...");
         gañán.speak();
 
-        System.out.println("¡Ignición!");
+        regulator.regulate(gañán, moza, minTemp, maxTemp, temperature, 1.5);
+        /* In the line of code above, it is demonstrated that dependency injection works with 2 different objects
+        with the same interface implementation.
+         */
+        System.out.println('\n' + "¡Ignición!");
         moza.speak();
+
+        /**
+         * The man says:
+         *    Let's do it!
+         *
+         * Calentando => temperatura 16.5
+         * Calentando => temperatura 18.0
+         * Calentando => temperatura 19.5
+         * Calentando => temperatura 21.0
+         * Apagado => temperatura 19.5
+         * Apagado => temperatura 18.0
+         * Apagado => temperatura 16.5
+         * Apagado => temperatura 15.0
+         *
+         * ¡Ignición!
+         * The girl says:
+         *    Oh my gosh!
+         */
     }
 }

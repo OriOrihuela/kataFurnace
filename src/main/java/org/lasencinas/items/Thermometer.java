ca6167343f522e2b5f17895acb4c244c41d78539
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-04-05 20:14:52 +0200

    Methods implemented.

--- src/main/java/org/lasencinas/items/Thermometer.java
+++ src/main/java/org/lasencinas/items/Thermometer.java
@@ -3,4 +3,10 @@ package org.lasencinas.items;
 import org.lasencinas.interfaces.Temperable;
 
 public class Thermometer implements Temperable {
+
+    /* ---- Behaviours ---- */
+    @Override
+    public void read() {
+
+    }
 }

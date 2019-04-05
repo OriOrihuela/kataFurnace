aad8ad0973e9d93d39cd3971ede6aa418472482e
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-04-05 20:14:40 +0200

    Class replaced into person package.

---
+++ src/main/java/org/lasencinas/person/Girl.java
@@ -0,0 +1,22 @@
+package org.lasencinas.person;
+
+import org.lasencinas.interfaces.Heatable;
+
+public class Girl implements Heatable {
+
+    /* ---- Behaviours ---- */
+
+    @Override
+    public void disengage() {
+
+    }
+
+    @Override
+    public void engage() {
+
+    }
+
+    public void speak() {
+        System.out.println("Oh my gosh!");
+    }
+}

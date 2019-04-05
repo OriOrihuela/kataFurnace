6e556bbf89aa8b810b2b8306db097ff3cc9ffd64
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-04-05 21:53:34 +0200

    Override deleted.

--- src/main/java/org/lasencinas/items/Thermometer.java
+++ src/main/java/org/lasencinas/items/Thermometer.java
@@ -3,10 +3,4 @@ package org.lasencinas.items;
 import org.lasencinas.interfaces.Temperable;
 
 public class Thermometer implements Temperable {
-
-    /* ---- Behaviours ---- */
-    @Override
-    public void read() {
-
-    }
 }

635461fde9ad00aabc3dd4327f6db0fea68abe4e
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-04-05 19:47:24 +0200

    RoomTemperature done with Singleton Pattern.

---
+++ src/main/java/org/lasencinas/room/RoomTemperature.java
@@ -0,0 +1,25 @@
+package org.lasencinas.room;
+
+public class RoomTemperature {
+    /* ---- Properties of the class ---- */
+    private final RoomTemperature INSTANCE = new RoomTemperature();
+    private double temperature = 0d;
+
+
+    /* ---- Constructor ---- */
+    public RoomTemperature() {}
+
+    public RoomTemperature(double temperature) {
+        this.temperature = temperature;
+    }
+
+
+    /* ---- Getters ---- */
+    public RoomTemperature getInstance() {
+        return INSTANCE;
+    }
+
+    public double getTemperature() {
+        return temperature;
+    }
+}

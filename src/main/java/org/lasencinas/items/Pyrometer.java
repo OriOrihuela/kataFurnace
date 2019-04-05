ca3b8cee165a251540b7ac91623884f7a920af83
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-04-05 20:13:19 +0200

    RoomTemperature done with Singleton Pattern.

--- src/main/java/org/lasencinas/items/Pyrometer.java
+++ src/main/java/org/lasencinas/items/Pyrometer.java
@@ -3,4 +3,10 @@ package org.lasencinas.items;
 import org.lasencinas.interfaces.Temperable;
 
 public class Pyrometer implements Temperable {
+
+    /* ---- Behaviours ---- */
+    @Override
+    public void read() {
+
+    }
 }

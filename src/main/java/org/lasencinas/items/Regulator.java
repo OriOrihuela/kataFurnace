2245c699dec25289e689907def9fe2a2058491f1
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-04-05 21:54:47 +0200

    regulate() and message() implemented.

--- src/main/java/org/lasencinas/items/Regulator.java
+++ src/main/java/org/lasencinas/items/Regulator.java
@@ -1,5 +1,6 @@
 package org.lasencinas.items;
 
+import org.lasencinas.codes.Codes;
 import org.lasencinas.interfaces.Heatable;
 import org.lasencinas.interfaces.Regulable;
 import org.lasencinas.interfaces.Temperable;
@@ -9,7 +10,31 @@ public class Regulator implements Regulable {
 
     /* ---- Behaviours ---- */
     public void regulate(Temperable temperable, Heatable heatable, double minTemp, double maxTemp,
-                         RoomTemperature roomTemperature) {
+                         RoomTemperature roomTemperature, double grades) {
+        Codes code;
+        while (temperable.read(roomTemperature) < maxTemp) {
+            code = Codes.HEATING;
+            heatable.engage(roomTemperature, grades);
+            message(code, roomTemperature);
+        }
+        while (temperable.read(roomTemperature) > minTemp) {
+            code = Codes.WAITING;
+            heatable.disengage(roomTemperature, grades);
+            message(code, roomTemperature);
+        }
+    }
 
+    private void message(Codes code, RoomTemperature temperature) {
+        switch (code) {
+            case HEATING:
+                System.out.println("Calentando => temperatura " + temperature.getTemperature());
+                break;
+            case WAITING:
+                System.out.println("Apagado => temperatura " + temperature.getTemperature());
+                break;
+            default:
+                System.out.println("Algo raro sucede...");
+                break;
+        }
     }
 }

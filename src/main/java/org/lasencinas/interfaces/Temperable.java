baf9d49e9e9703965babe3a8708ee65bb1de8064
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-04-05 21:53:51 +0200

    read() method made as default.

--- src/main/java/org/lasencinas/interfaces/Temperable.java
+++ src/main/java/org/lasencinas/interfaces/Temperable.java
@@ -1,5 +1,11 @@
 package org.lasencinas.interfaces;
 
+import org.lasencinas.room.RoomTemperature;
+
 public interface Temperable {
-    void read();
+
+    /* ---- Behaviours ---- */
+    default double read(RoomTemperature roomTemperature){
+        return roomTemperature.getTemperature();
+    }
 }

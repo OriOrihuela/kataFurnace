6a0cc75d5c721fb9daa20e5cef34d8caafce3614
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-04-07 16:23:07 +0200

    regulate() converted as default method.

--- src/main/java/org/lasencinas/interfaces/Regulable.java
+++ src/main/java/org/lasencinas/interfaces/Regulable.java
@@ -3,6 +3,10 @@ package org.lasencinas.interfaces;
 import org.lasencinas.room.RoomTemperature;
 
 public interface Regulable {
-    void regulate(Temperable temperable, Heatable heatable, double minTemp, double maxTemp,
-                  RoomTemperature roomTemperature, double grades);
+
+    /* ---- Behaviours ---- */
+    default void regulate(Temperable temperable, Heatable heatable, double minTemp, double maxTemp,
+                  RoomTemperature roomTemperature) {
+        // The body of the method that each regulator will overwrite according to its operation.
+    }
 }
